fd3b0207fc88c915aee26ca89bcbf2d0c5e0f86b
huangwh <huangweihuang18@163.com>
2019-04-09 18:36:48 +0800

    网关和配置

---
+++ hug-config/src/main/java/com/hug/config/HugConfigApplication.java
@@ -0,0 +1,13 @@
+package com.hug.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HugConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HugConfigApplication.class, args);
+    }
+
+}
