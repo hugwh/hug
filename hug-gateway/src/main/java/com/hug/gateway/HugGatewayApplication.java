af818775df5537a5d8c211c8f0a5ed238586373a
huangwh <huangweihuang18@163.com>
2019-04-17 10:31:39 +0800

    模块配置从远程拉取配置文件

--- hug-gateway/src/main/java/com/hug/gateway/HugGatewayApplication.java
+++ hug-gateway/src/main/java/com/hug/gateway/HugGatewayApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableEurekaClient
 @SpringCloudApplication
 public class HugGatewayApplication {
 
