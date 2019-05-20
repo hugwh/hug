3a01b7661fcf742875ac76c9588dc4265a75a2a6
hug <hwh2coding@163.com>
2019-05-20 22:23:09 +0800

    功能码设计优化

--- hug-common/src/main/java/com/hug/common/exception/SystemException.java
+++ hug-common/src/main/java/com/hug/common/exception/SystemException.java
@@ -1,9 +1,14 @@
 package com.hug.common.exception;
 
+import com.hug.common.constant.ResultConstants;
+
 /**
  * 系统异常
  * @author: huangwh
  * @date: 2019/5/20 21:33
  */
-public class SystemException {
+public class SystemException extends ResultException {
+    public SystemException(String type, String msg) {
+        super(ResultConstants.CODE_SYS_ERR, type, msg);
+    }
 }
